5508349f987783ac384eadc6ca76d45db97a708c
Carlos Tovar <cartovarc@gmail.com>
2021-01-27 09:39:10 -0500

    add template

---
+++ src/components/FooterAppLanding.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="app-landing-bottom-banner-area app-bottom-bg  drak-area theme_bg-2">
+        <div class="vc_row-separator center_curve top">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" preserveAspectRatio="none" viewBox="0 0 100 100">
+                <path d="M 0 0 Q 50 200 100 0"></path>
+            </svg>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="bottom-banner-images text-center">
+                        <img src="@/assets/img/features/aeroland-payment-image-03.png" class="img-fluid" alt="image">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-area-wrapper">
+            <div class="footer-area section-space--pt_90 section-space--pb_60">
+                <div class="container">
+                    <div class="row footer-widget-wrapper">
+                        <div class="col-lg-12">
+                            <div class="app-landing_footer text-center">
+                                <h2 class="heading font-weight--bold text-white">Download Our App Today!</h2>
+                                <h6 class="sub-title mt-20 font-weight--light text-white">We’ll help you achieve your marketing &amp; business goals</h6>
+                                <div class="app-landing-background__links section-space--mt_50">
+                                    <a href="https://play.google.com/" target="_blank">
+                                        <img src="../assets/img/icons/aeroland-button-google-play.jpg" class="img-fluid" alt="image">
+                                    </a>
+                                    <a href="https://www.apple.com/ios/app-store/" target="_blank">
+                                        <img src="../assets/img/icons/aeroland-button-app-store.jpg" class="img-fluid" alt="image">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright-area section-space--ptb_60">
+                <div class="container">
+                    <div class="row aling-items-center">
+                        <div class="col-md-12">
+                            <div class="horizontal-list text-center">
+                                <ul>
+                                    <li>© 2020s AeroLand. All Rights Reserved</li>
+                                    <li>
+                                        <a href="tel:(+00)123567990" class="hover-style-link hover-style-link--white">(+00) 123 567990</a>
+                                    </li>
+                                    <li>
+                                        <a href="mailto:contact@aeroland.com" class="hover-style-link hover-style-link--white">contact@aeroland.com </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="app-landing_footer-share text-center">
+                                <ul class="list ht-social-networks large-icon">
+                                    <li class="item">
+                                        <a href="https://www.facebook.com/" target="_blank" class="social-link"> <i class="fab fa-facebook social-link-icon"></i></a>
+                                    </li>
+                                    <li class="item">
+                                        <a href="https://twitter.com/" target="_blank" class="social-link"> <i class="fab fa-twitter social-link-icon"></i></a>
+                                    </li>
+                                    <li class="item">
+                                        <a href="https://www.instagram.com/" target="_blank" class="social-link"> <i class="fab fa-instagram social-link-icon"></i></a>
+                                    </li>
+                                    <li class="item">
+                                        <a href="https://dribbble.com/" target="_blank" class="social-link"> <i class="fab fa-dribbble social-link-icon"></i></a>
+                                    </li>
+                                    <li class="item">
+                                        <a href="https://www.pinterest.com/" target="_blank" class="social-link"> <i class="fab fa-pinterest social-link-icon"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/FooterBottom.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="footer-area-wrapper">
+        <div class="footer-copyright-area ebook-footer section-space--pb_120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="ebook-social-group__list mb-20 text-center">
+                            <ul class="list ht-social-networks tilte-style">
+                                <li class="item">
+                                    <a href="https://twitter.com/" target="_blank" class="social-link">
+                                        <span class="link-text">Twitter</span> 
+                                    </a>
+                                </li>
+                                <li class="item">
+                                    <a href="https://dribbble.com/" target="_blank" class="social-link">
+                                        <span class="link-text">Dribbble</span> 
+                                    </a>
+                                </li>
+                                <li class="item">
+                                    <a href="https://www.instagram.com/" target="_blank" class="social-link">
+                                        <span class="link-text">Instagram</span>
+                                    </a>
+                                </li>
+                                <li class="item">
+                                    <a href="https://www.linkedin.com/" target="_blank" class="social-link">
+                                        <span class="link-text">LinkedIn</span>
+                                    </a>
+                                </li>
+                                <li class="item">
+                                    <a href="#" target="_blank" class="social-link">
+                                        <span class="link-text">Recommended Books</span>
+                                     </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row aling-items-center">
+                    <div class="col-md-12">
+                        <div class="horizontal-list text-center">
+                            <ul>
+                                <li>© 2020s AeroLand. All Rights Reserved</li>
+                                <li>
+                                    <a href="tel:(+00)123567990" class="hover-style-link hover-style-link--sub-color">(+00) 123 567990</a>
+                                </li>
+                                <li>
+                                    <a href="mailto:contact@aeroland.com" class="hover-style-link hover-style-link--sub-color">contact@aeroland.com</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
