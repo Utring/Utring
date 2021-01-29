b0864ec4ab4664194cafcc293cd6abadaaf226e6
Diego Calderon Andrade <73897850+dcaland@users.noreply.github.com>
2021-01-29 15:42:55 -0500

    se traduce y actualiza informacion en FooterMain.vue y CarouselStartup.vue

--- src/components/FooterMain.vue
+++ src/components/FooterMain.vue
@@ -6,17 +6,17 @@
                     <div class="col-lg-4 col-md-6 footer-widget">
                         <div class="footer-widget__logo mb-30">
                             <router-link to="/">
-                                <img src="../assets/img/logo/logo-dark.png" class="img-fluid logo-dark" alt="logo">
-                                <img src="../assets/img/logo/logo-light.png" class="img-fluid logo-light" alt="logo">
+                                <img src="../assets/img/logo/logo-dark-1.png" class="img-fluid logo-dark" alt="logo">
+                                <img src="../assets/img/logo/light-logo-1.png" class="img-fluid logo-light" alt="logo">
                             </router-link>
                         </div>
                         <ul class="footer-widget__list">
-                            <li>58 Howard Street #2 San Francisco, CA 941</li>
+                            <li>Calle 4 # 34 29, San Fernando, Cali Valle Colombia</li>
                             <li>
-                                <a href="mailto:contact@aeroland.com" class="hover-style-link">contact@aeroland.com</a>
+                                <a href="mailto:contact@aeroland.com" class="hover-style-link">contacto@utring.com</a>
                             </li>
                             <li>
-                                <a href="tel:(+68)122109876" class="hover-style-link">(+68)1221 09876</a>
+                                <a href="tel:(+68)122109876" class="hover-style-link">(+57)300 0000000</a>
                             </li>
                             <li>
                                 <a href="www.website.com" target="_blank" class="hover-style-link hover-style-link--green">www.website.com</a>
@@ -24,36 +24,36 @@
                         </ul>
                     </div>
                     <div class="col-lg-2 col-md-6 footer-widget">
-                        <h6 class="footer-widget__title mb-20">Who we are</h6>
+                        <h6 class="footer-widget__title mb-20">Quienes somos</h6>
                         <ul class="footer-widget__list">
                             <li>
-                                <router-link to="/" class="hover-style-link">About us</router-link>
+                                <router-link to="/" class="hover-style-link">Sobre nosotros</router-link>
                             </li>
                             <li>
-                                <router-link to="/" class="hover-style-link">Before you go</router-link>
+                                <router-link to="/" class="hover-style-link">Antes de que te vayas</router-link>
                             </li>
                             <li>
-                                <router-link to="/" class="hover-style-link">Online check in</router-link>
+                                <router-link to="/" class="hover-style-link">Registro en Linea</router-link>
                             </li>
                             <li>
-                                <router-link to="/" class="hover-style-link">FAQ</router-link>
+                                <router-link to="/" class="hover-style-link">Preguntas más frecuentes</router-link>
                             </li>
                         </ul>
                     </div>
                     <div class="col-lg-2 col-md-6 footer-widget">
-                        <h6 class="footer-widget__title mb-20">Quick links</h6>
+                        <h6 class="footer-widget__title mb-20">Enlaces Rápidos</h6>
                         <ul class="footer-widget__list">
                             <li>
-                                <router-link to="/" class="hover-style-link">Pick up locations</router-link>
+                                <router-link to="/" class="hover-style-link">Nuestras Sedes</router-link>
                             </li>
                             <li>
-                                <router-link to="/" class="hover-style-link">Terms of Payment</router-link>
+                                <router-link to="/" class="hover-style-link">Condiciones de Pago</router-link>
                             </li>
                             <li>
-                                <router-link to="/" class="hover-style-link">Privacy Policy</router-link>
+                                <router-link to="/" class="hover-style-link">Políticas de Privacidad</router-link>
                             </li>
                             <li>
-                                <router-link to="/" class="hover-style-link">Where to Find Us</router-link>
+                                <router-link to="/" class="hover-style-link">Dónde Encontrarnos</router-link>
                             </li>
                         </ul>
                     </div>
@@ -64,13 +64,13 @@
                                     <div class="swiper-slide twitter-feed-slider__single">
                                         <div class="tweet">
                                             <div class="tweet__text">
-                                                "The ocean never ceases to amaze!" Feature: Ben Klea
-                                                <a href="https://t.co/jSRMsZAdPW" target="_blank">https://t.co/jSRMsZAdPW</a>
-                                                <a href="https://t.co/2iDReuyPMt" target="_blank">https://t.co/2iDReuyPMt</a>
+                                                Si el código y los comentarios no coinciden, posiblemente ambos sean incorrectos. – Norm Schryer
+                                                <!-- <a href="https://t.co/jSRMsZAdPW" target="_blank">https://t.co/jSRMsZAdPW</a>
+                                                <a href="https://t.co/2iDReuyPMt" target="_blank">https://t.co/2iDReuyPMt</a> -->
                                             </div>
                                             <div class="tweet__info">
-                                                <h6 class="tweet__heading">Unsplash</h6>
-                                                <span class="tweet__date">May 07, 2020</span>
+                                                <h6 class="tweet__heading">Utring</h6>
+                                                <span class="tweet__date">Enero 28, 2021</span>
                                             </div>
                                         </div>
                                     </div>
@@ -85,7 +85,7 @@
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-md-6 text-center text-md-left">
-                        <span class="copyright-text">© 2020 AeroLand. All Rights Reserved.</span>
+                        <span class="copyright-text">© 2021 Utring. All Rights Reserved.</span>
                     </div>
                     <div class="col-md-6 text-center text-md-right">
                         <ul class="list ht-social-networks solid-rounded-icon">
