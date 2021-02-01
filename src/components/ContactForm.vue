c1b75b96dcf7c3c7fc765419b55a8e45e7debc81
Carlos Tovar <cartovarc@gmail.com>
2021-02-01 16:44:46 -0500

    form testing

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,51 +1,95 @@
 <template>
-    <div class="contact-form-section section-space--ptb_120" id="contact">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-7 order-2 order-lg-1">
-                    <div class="section-title text-left section-space--mb_60">
-                        <h2 class="font-weight--bold mb-15 wow move-up">¡Cooperemos!</h2>
-                        <span class="section-text_left wow move-up">Seamos socios para construir un mejor futuro.</span>
-                    </div>
-                    <div class="contact-from-wrapper wow move-up">
-                        <form id="contact-form" action="http://hasthemes.com/file/mail.php" method="post">
-                            <div class="contact-page-form">
-                                <div class="contact-input">
-                                    <div class="contact-inner">
-                                        <input name="con_name" type="text" placeholder="Nombre *">
-                                    </div>
-                                    <div class="contact-inner">
-                                        <input name="con_email" type="email" placeholder="Email *">
-                                    </div>
-                                </div>
-                                <div class="contact-inner">
-                                    <input name="con_subject" type="text" placeholder="Asunto *">
-                                </div>
-                                <div class="contact-inner contact-message">
-                                    <textarea name="con_message" placeholder="Mensaje *"></textarea>
-                                </div>
-                                <div class="contact-submit-btn">
-                                    <button class="ht-btn ht-btn-md" type="submit">Enviar mensaje</button>
-                                    <p class="form-messege"></p>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+  <div class="contact-form-section section-space--ptb_120" id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 order-2 order-lg-1">
+          <div class="section-title text-left section-space--mb_60">
+            <h2 class="font-weight--bold mb-15 wow move-up">¡Cooperemos!</h2>
+            <span class="section-text_left wow move-up"
+              >Seamos socios para construir un mejor futuro.</span
+            >
+          </div>
+          <div class="contact-from-wrapper wow move-up">
+            <form @submit.prevent="submitForm">
+              <div class="contact-page-form">
+                <div class="contact-input">
+                  <div class="contact-inner">
+                    <input v-model="name" type="text" placeholder="Nombre *" />
+                  </div>
+                  <div class="contact-inner">
+                    <input v-model="email" type="email" placeholder="Email *" />
+                  </div>
                 </div>
-                <div class="col-lg-5 order-1 order-lg-2">
-                    <div class="peatures_image-wrap section-space--mb_40">
-                        <div class="image text-center wow move-up">
-                            <img class="img-fluid" src="../assets/img/features/aeroland-contact-form-image-01.png" alt="contact thumb">
-                        </div>
-                    </div>
+                <div class="contact-inner">
+                  <input v-model="subject" type="text" placeholder="Asunto *" />
                 </div>
-            </div>
+                <div class="contact-inner contact-message">
+                  <textarea v-model="text" placeholder="Mensaje *"></textarea>
+                </div>
+                <div class="contact-submit-btn">
+                  <button class="ht-btn ht-btn-md" type="submit">
+                    Enviar mensaje
+                  </button>
+                  <p class="form-messege"></p>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
+        <div class="col-lg-5 order-1 order-lg-2">
+          <div class="peatures_image-wrap section-space--mb_40">
+            <div class="image text-center wow move-up">
+              <img
+                class="img-fluid"
+                src="../assets/img/features/aeroland-contact-form-image-01.png"
+                alt="contact thumb"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'ContactForm'
-    }
+
+const axios = require("axios");
+
+export default {
+  name: "ContactForm",
+  data() {
+    return {
+      name: null,
+      email: null,
+      subject: null,
+      text: null,
+    };
+  },
+  methods: {
+    submitForm() {
+      let data = {
+        name: this.name,
+        email: this.email,
+        subject: this.subject,
+        text: this.text,
+      };
+      console.log(data);
+      axios
+        .post("http://utring.herokuapp.com/api/send-email", data)
+        .then((response) => {
+          // console.log(response);
+          // this.response = response.data
+          this.success = "Data saved successfully";
+          this.response = JSON.stringify(response, null, 2);
+        })
+        .catch((error) => {
+          this.response = "Error: " + error.response.status;
+        });
+      this.name = "";
+      this.email = "";
+      this.firstSon = "";
+    },
+  },
+};
 </script>
